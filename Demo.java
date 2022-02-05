14323a6bc7f5743a2b1c03b6fd553d0837d20406
Amol Kumbhare <amol2431980@gmail.com>
2022-02-05 21:02:41 +0530

    First Commit

--- Demo.java
+++ Demo.java
@@ -1,6 +1,9 @@
 public class Demo {
 
-    public static void main(String args[]){
+    public static void main(String[] args){
+
         System.out.println("Hi Amol");
+        System.out.println("hi Dear Alakh");
     }
+
 }
